9eee9c2663fcdfb2399ea628fb185d3711c001da
LEONARDOX <42386676+LEONARD0X@users.noreply.github.com>
2021-01-06 13:49:12 +0100

    Add comments

--- src/main/java/de/leonardox/gueststreams/GuestStreams.java
+++ src/main/java/de/leonardox/gueststreams/GuestStreams.java
@@ -8,54 +8,68 @@ import org.javacord.api.entity.channel.ServerChannelUpdater;
 import org.javacord.api.entity.channel.ServerVoiceChannel;
 import org.javacord.api.entity.permission.PermissionState;
 import org.javacord.api.entity.permission.PermissionType;
-import org.javacord.api.entity.permission.Permissions;
 import org.javacord.api.entity.permission.PermissionsBuilder;
 
 public class GuestStreams implements Consumer<DiscordApi> {
 
+	// Your token here
 	public static final String TOKEN = "";
+
+	@Override
+	public void accept(DiscordApi api) {
+		api.addServerVoiceChannelMemberJoinListener(event -> {
+			ServerVoiceChannel channel = event.getChannel();
+
+			// Check if bot should operate in this channel
+			if (!channel.getOverwrittenPermissions().containsKey(api.getClientId())) {
+				return;
+			}
+
+			PermissionsBuilder builder = null;
+			if (channel.getOverwrittenPermissions().containsKey(event.getUser().getId())) {
+				builder = new PermissionsBuilder(channel.getOverwrittenPermissions().get(event.getUser().getId()));
+			} else {
+				builder = new PermissionsBuilder();
+			}
+
+			// Indicates that the member already has the permission to view this channel
+			if (builder.getState(PermissionType.CONNECT) == PermissionState.ALLOWED) {
+				return;
+			}
+
+			// Add temporary permissions to view and start steams
+			builder.setState(PermissionType.STREAM, PermissionState.ALLOWED);
+			builder.setState(PermissionType.READ_MESSAGES, PermissionState.ALLOWED);
+			new ServerChannelUpdater(channel).addPermissionOverwrite(event.getUser(), builder.build()).update();
+		});
+
+		api.addServerVoiceChannelMemberLeaveListener(event -> {
+			ServerVoiceChannel channel = event.getChannel();
+
+			// Check if bot should operate in this channel
+			if (!channel.getOverwrittenPermissions().containsKey(api.getClientId())) {
+				return;
+			}
+
+			PermissionsBuilder builder = null;
+			if (channel.getOverwrittenPermissions().containsKey(event.getUser().getId())) {
+				builder = new PermissionsBuilder(channel.getOverwrittenPermissions().get(event.getUser().getId()));
+			} else {
+				builder = new PermissionsBuilder();
+			}
+
+			// Indicates that the member does not have temporary permissions
+			if (builder.getState(PermissionType.CONNECT) == PermissionState.ALLOWED) {
+				return;
+			}
+
+			// Remove temporary permissions
+			new ServerChannelUpdater(channel).removePermissionOverwrite(event.getUser()).update();
+		});
+	}
 
 	public static void main(String[] args) {
 		new DiscordApiBuilder().setToken(TOKEN).login().thenAccept(new GuestStreams());
 	}
 
-	@Override
-	public void accept(DiscordApi api) {
-		api.addServerVoiceChannelMemberJoinListener(event -> {
-			ServerVoiceChannel ch = event.getChannel().asServerVoiceChannel().get();
-			if (!ch.getOverwrittenPermissions().containsKey(api.getClientId())) {
-				return;
-			}
-			PermissionsBuilder builder = null;
-			if (ch.getOverwrittenPermissions().containsKey(event.getUser().getId())) {
-				builder = new PermissionsBuilder(ch.getOverwrittenPermissions().get(event.getUser().getId()));
-			} else {
-				builder = new PermissionsBuilder();
-			}
-			if (builder.getState(PermissionType.CONNECT) == PermissionState.ALLOWED) {
-				return;
-			}
-			builder.setState(PermissionType.STREAM, PermissionState.ALLOWED);
-			builder.setState(PermissionType.READ_MESSAGES, PermissionState.ALLOWED);
-			Permissions perm = builder.build();
-			new ServerChannelUpdater(ch).addPermissionOverwrite(event.getUser(), perm).update();
-		});
-
-		api.addServerVoiceChannelMemberLeaveListener(event -> {
-			ServerVoiceChannel ch = event.getChannel().asServerVoiceChannel().get();
-			if (!ch.getOverwrittenPermissions().containsKey(api.getClientId())) {
-				return;
-			}
-			PermissionsBuilder builder = null;
-			if (ch.getOverwrittenPermissions().containsKey(event.getUser().getId())) {
-				builder = new PermissionsBuilder(ch.getOverwrittenPermissions().get(event.getUser().getId()));
-			} else {
-				builder = new PermissionsBuilder();
-			}
-			if (builder.getState(PermissionType.CONNECT) == PermissionState.ALLOWED) {
-				return;
-			}
-			new ServerChannelUpdater(ch).removePermissionOverwrite(event.getUser()).update();
-		});
-	}
 }
